bddb0af3ca345e2d9b216c2d37328b7dc04412a9
Ivan Kalinichenko <kalinichenko@constlab.ru>
2020-04-18 15:01:05 +0300

    chore: blocks palette search

--- src/components/blocks-palette/blocks-palette-search.tsx
+++ src/components/blocks-palette/blocks-palette-search.tsx
@@ -2,7 +2,50 @@ import Vue, { VNode } from 'vue'
 
 export default Vue.extend({
   name: 'BlocksPaletteSearch',
+  data() {
+    return {
+      search: '' as string,
+    }
+  },
+  methods: {
+    focus(): void {
+      ;(this.$refs['input'] as HTMLInputElement).focus()
+    },
+    clear(): void {
+      this.search = ''
+      this.$emit('change', '')
+    },
+  },
   render(): VNode {
-    return <div>search</div>
+    let clearButton: VNode | undefined
+    if (this.search !== '') {
+      clearButton = (
+        <q-icon
+          name="close"
+          class="cursor-pointer"
+          on-click={() => {
+            this.clear()
+            this.focus()
+          }}
+        />
+      )
+    }
+
+    return (
+      <q-input
+        ref="input"
+        label="Search"
+        filled={true}
+        dark={true}
+        value={this.search}
+        scopedSlots={{
+          append: () => [clearButton, <q-icon name="search" />],
+        }}
+        on-input={(value: string) => {
+          this.search = value
+          this.$emit('change', value.trim())
+        }}
+      />
+    )
   },
 })
